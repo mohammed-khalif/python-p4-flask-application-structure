5aef914075cd175b5b6f8890de225eb8bb295c80
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-06-19 12:15:50 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -3,3 +3,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# append to app/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+# append to app/app.py
+
+@app.route('/<username>')
+def users(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# modify user() in app/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
